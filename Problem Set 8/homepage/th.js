dbc5586def07f2f65940f09b4b8480bc287473f8
Md. Nazmul Islam Rumman <142523457+pomegstar@users.noreply.github.com>
2024-04-23 22:11:17 -0700

    Create th.js

---
+++ Problem Set 8/homepage/th.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+                let al = document.querySelector('#bd');
+                al.onclick = function(){
+                    alert('You are a rules breaker. You did not follow my instruction.');
+                }
+                let rg = document.querySelector('#f');
+                rg.onclick = function(){
+                    alert('Thanks for sharing...');
+                }
+});
