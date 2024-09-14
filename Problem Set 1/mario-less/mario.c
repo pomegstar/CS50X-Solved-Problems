86ac8ee3eb51c5f2b44ea394a5ecbc33f4ff3415
Md. Nazmul Islam Rumman <142523457+pomegstar@users.noreply.github.com>
2024-09-14 07:44:21 -0700

    Add files via upload

---
+++ Problem Set 1/mario-less/mario.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int spaces, int bricks);
+
+int main(void)
+{
+    // Prompt the user for the pyramid's height
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1);
+
+    // Print a pyramid of that height
+    for (int i = 0; i < n; i++)
+    {
+        // Print row of bricks
+        print_row(n - i - 1, i + 1);
+    }
+}
+
+void print_row(int spaces, int bricks)
+{
+    // Print spaces
+    for (int i = 0; i < spaces; i++)
+    {
+        printf(" ");
+    }
+
+    // Print bricks
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#");
+    }
+
+    // Move to the next line
+    printf("\n");
+}
